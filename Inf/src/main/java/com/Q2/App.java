687b21d8898afded6d098407813787d7b73b8f6b
Julius <113830349+Juliorocktect@users.noreply.github.com>
2024-08-23 10:03:01 +0000

    rundreise

---
+++ Inf/src/main/java/com/Q2/App.java
@@ -0,0 +1,15 @@
+package com.Q2;
+import com.Q2.lw1.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Karte karte = new Karte();
+        karte.starteRundreise("L");
+        System.out.println( "Hello World!" );
+    }
+}
